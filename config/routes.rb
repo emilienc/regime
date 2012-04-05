2c3f20e660e09ffc445c88f0fe6ae22861693ce3
emilien <emilien.charbonnier@cegetel.net>
2012-04-05 07:36:28 +0200

    authent

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Regime::Application.routes.draw do
+  get "welcome/index"
+
+  resources :sessions
+  resources :users
   resources :pesees
 
   # The priority is based upon order of creation:
@@ -50,7 +54,7 @@ Regime::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120404051008) do
+ActiveRecord::Schema.define(:version => 20120405051900) do
 
   create_table "pesees", :force => true do |t|
     t.date     "quand"
     t.decimal  "combien"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
 end
